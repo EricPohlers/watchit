d7d614b108c3caa2173442b640958f5523f95163
Eric Pohlers <e.pohlers@web.de>
2020-05-14 11:22:38 +0200

    add errorhandling

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
+const fs = require('fs');
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => {
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
     const start = debounce(() => {
       console.log('STARTING USERS PROGRAM');
     }, 300);
