5f96e8fd92848b3f01541986b3532d032a6a21d3
Eric Pohlers <e.pohlers@web.de>
2020-05-14 11:17:04 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action((args) => {
+    const start = debounce(() => {
+      console.log('STARTING USERS PROGRAM');
+    }, 300);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
